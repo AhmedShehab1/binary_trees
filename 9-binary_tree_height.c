7907459ae68cbf02d1b6b60637ce29028e803afa
MazenAtlam <mazenatef5510@gmail.com>
2024-06-18 02:07:29 +0300

    Task 9 version 2.0

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,48 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * height_from_right - A function that measures the height of a binary tree
- * from right
- *
- * @tree: A pointer to the root node of the tree to traverse
- *
- * Return: The height from right
- */
-size_t height_from_right(const binary_tree_t *tree)
-{
-	binary_tree_t *curr_tree = tree->right;
-	size_t height = 0;
-
-	while (curr_tree != NULL)
-	{
-		height++;
-		curr_tree = curr_tree->right;
-	}
-	return (height);
-}
-
-/**
- * height_from_left - A function that measures the height of a binary tree
- * from left
- *
- * @tree: A pointer to the root node of the tree to traverse
- *
- * Return: The height from left
- */
-size_t height_from_left(const binary_tree_t *tree)
-{
-	binary_tree_t *curr_tree = tree->left;
-	size_t height = 0;
-
-	while (curr_tree != NULL)
-	{
-		height++;
-		curr_tree = curr_tree->left;
-	}
-	return (height);
-}
-
-/**
  * binary_tree_height - A function that measures the height of a binary tree
  *
  * @tree: A pointer to the root node of the tree to traverse
@@ -57,8 +15,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	r_height = height_from_right(tree);
-	l_height = height_from_left(tree);
+	r_height = 1 + binary_tree_height(tree->right);
+	l_height = 1 + binary_tree_height(tree->left);
 
 	if (r_height >= l_height)
 		return (r_height);
